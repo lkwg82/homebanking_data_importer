78c393b937422ac7dc6799773ff115d8325494cf
Lars K.W. Gohlke <lkwg82@gmx.de>
2024-09-04 07:49:57 +0200

    adds checks

--- src/main/java/de/lgohlke/homebanking/AccountStatus.java
+++ src/main/java/de/lgohlke/homebanking/AccountStatus.java
@@ -1,5 +1,6 @@
 package de.lgohlke.homebanking;
 
+import lombok.NonNull;
 import lombok.SneakyThrows;
 import nl.garvelink.iban.IBAN;
 
@@ -9,19 +10,23 @@ import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.util.Locale;
 
-public record AccountStatus(Date date, IBAN iban, BigDecimal balance, String name) {
-    public static AccountStatus parse(String iban, String balance, String name) {
-
+public record AccountStatus(@NonNull Date date, IBAN iban, BigDecimal balance, String name) {
+    public static AccountStatus parse(@NonNull String iban, @NonNull String balance, @NonNull String name) {
         Date now = Date.valueOf(LocalDate.now());
         return parse(now, iban, balance, name);
     }
 
-    public static AccountStatus parse(Date date, String iban, String balance, String name) {
+    public static AccountStatus parse(@NonNull Date date, @NonNull String iban, @NonNull String balance, String name) {
         return new AccountStatus(date, IBAN.valueOf(iban), parseBalance(balance), name);
     }
 
     @SneakyThrows
     private static BigDecimal parseBalance(String balanceString) {
+
+        if (balanceString.isBlank()) {
+            throw new IllegalArgumentException("just blank balance str: '" + balanceString + "'");
+        }
+
         NumberFormat format = NumberFormat.getInstance(Locale.GERMANY);
         Number number = format.parse(balanceString.replace(" €", ""));
         return new BigDecimal(number.toString());
--- src/test/java/de/lgohlke/homebanking/AccountStatusTest.java
+++ src/test/java/de/lgohlke/homebanking/AccountStatusTest.java
@@ -6,18 +6,28 @@ import java.sql.Date;
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AccountStatusTest {
+    private final String IBAN = "DE75 5001 0517 2221 8623 18";
+
     @Test
     void test_account_status() {
         Date date = Date.valueOf(LocalDate.of(2024, 8, 27));
 
-        AccountStatus status = AccountStatus.parse(date, "DE75 5001 0517 2221 8623 18", "2.123,54 €",
+        AccountStatus status = AccountStatus.parse(date, IBAN, "2.123,54 €",
                                                    "Tagesgeld - Otto");
 
         assertThat(status.iban()
                          .toPlainString()).isEqualTo("DE75500105172221862318");
         assertThat(status.balance()).isEqualTo("2123.54");
         assertThat(status.name()).isEqualTo("Tagesgeld - Otto");
+    }
+
+    @Test
+    void fail_with_empty_balance() {
+        assertThatThrownBy(() -> AccountStatus.parse(IBAN, "", "test")
+                , "just blank str: ''"
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 }
