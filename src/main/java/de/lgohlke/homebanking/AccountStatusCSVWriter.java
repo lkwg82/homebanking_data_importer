702e4f4aafcb46558775bd7e566ee930bb82a620
Lars K.W. Gohlke <lkwg82@gmx.de>
2024-09-07 13:58:30 +0200

    adds Summary

--- src/main/java/de/lgohlke/homebanking/AccountStatusCSVWriter.java
+++ src/main/java/de/lgohlke/homebanking/AccountStatusCSVWriter.java
@@ -4,22 +4,70 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileFilter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.Date;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RequiredArgsConstructor
 @Slf4j
 public class AccountStatusCSVWriter {
+    public static final String SUMMARY_CSV = "summary.csv";
     private final Path dataDirectory;
 
     public void writeStatusesToCSV(List<AccountStatus> statuses) {
         statuses.forEach(this::writeSingleStatusToCSV);
+    }
+
+    public void writeSummaryToCSV() {
+        List<AccountStatus> accountStatuses = collectStatuses();
+        var statusLines = accountStatuses.stream()
+                                         .map(AccountStatusCSVWriter::convertAccountStatusToCSVLine)
+                                         .collect(Collectors.joining("\n"));
+        try {
+            log.info("writing {}", SUMMARY_CSV);
+            Files.writeString(dataDirectory.resolve(SUMMARY_CSV), statusLines);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<AccountStatus> collectStatuses() {
+        FileFilter dirFilter = File::isDirectory;
+        FileFilter csvFileFilter = pathname -> pathname.isFile() && pathname.getPath().endsWith(".csv");
+        File[] dirs = dataDirectory.toFile().listFiles(dirFilter);
+        return Stream.of(Objects.requireNonNull(dirs))
+                     .map(dir -> dir.listFiles(csvFileFilter))
+                     .filter(Objects::nonNull)
+                     .flatMap(Arrays::stream)
+                     .map(file -> {
+                         try {
+                             List<String> lines = Files.readAllLines(file.toPath());
+                             if (lines.size() == 2) {
+                                 return lines.get(1);
+                             }
+                             throw new IllegalStateException("should have two lines");
+                         } catch (IOException e) {
+                             throw new RuntimeException(e);
+                         }
+                     }).map(line -> {
+                    String[] parts = line.split("\\|");
+                    if (parts.length == 4) {
+                        return AccountStatus.parse(Date.valueOf(parts[0]), parts[1], parts[2], parts[3]);
+                    }
+                    return null;
+                }).filter(Objects::nonNull).toList();
     }
 
     private void writeSingleStatusToCSV(AccountStatus status) {
@@ -32,7 +80,6 @@ public class AccountStatusCSVWriter {
         String fileName = createTimestampedFileName();
         Path filePath = folderPath.resolve(fileName);
 
-        // Schreibe die CSV-Datei
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath.toFile()))) {
             writer.write("Date|IBAN|Balance|Name");
             writer.newLine();
--- src/test/java/de/lgohlke/homebanking/AccountStatusFileWriterTest.java
+++ src/test/java/de/lgohlke/homebanking/AccountStatusFileWriterTest.java
@@ -14,7 +14,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountStatusFileWriterTest {
-
     @Test
     void should_write_account_statuses(@TempDir Path tempdir) throws IOException {
 
@@ -42,5 +41,25 @@ public class AccountStatusFileWriterTest {
                                                Date|IBAN|Balance|Name
                                                2024-08-27|DE75 5001 0517 2221 8623 18|2123.54|Giro1
                                                """);
+    }
+
+    @Test
+    void should_write_merged_summary_report(@TempDir Path tempdir) throws IOException {
+        Date date = Date.valueOf(LocalDate.of(2024, 8, 27));
+        List<AccountStatus> statuses = List.of(
+                AccountStatus.parse(date, "DE75 5001 0517 2221 8623 18", "2.123,54 €", "Giro1"),
+                AccountStatus.parse(date, "DE88 5001 0517 2235 7114 53", "5.123,54 €", "Giro2")
+        );
+
+        AccountStatusCSVWriter accountStatusCSVWriter = new AccountStatusCSVWriter(tempdir);
+        accountStatusCSVWriter.writeStatusesToCSV(statuses);
+
+        accountStatusCSVWriter.writeSummaryToCSV(); // test
+
+        Path summary = tempdir.resolve("summary.csv");
+        assertThat(summary).isNotEmptyFile();
+
+        List<String> lines = Files.readAllLines(summary);
+        assertThat(lines).hasSize(2);
     }
 }
