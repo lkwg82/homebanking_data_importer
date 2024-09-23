caf290e390c63c535b81cec684f35057747e6712
Lars K.W. Gohlke <lkwg82@gmx.de>
2024-09-23 07:46:35 +0200

    cleanup

--- src/main/java/de/lgohlke/homebanking/institutes/quirion/QuirionDataRetriever.java
+++ src/main/java/de/lgohlke/homebanking/institutes/quirion/QuirionDataRetriever.java
@@ -8,6 +8,7 @@ import de.lgohlke.homebanking.BrowserLauncher;
 import de.lgohlke.homebanking.DataFromBankRetriever;
 import de.lgohlke.homebanking.LoginCredential;
 import de.lgohlke.homebanking.institutes.BankingURL;
+import de.lgohlke.homebanking.institutes.InstitutePage;
 import de.lgohlke.homebanking.keepass.KeepassCredentialsRetriever;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,14 +19,14 @@ import java.util.List;
 public record QuirionDataRetriever(Path dataDirectory) implements DataFromBankRetriever {
     @Override
     public void fetchData() {
-        KeepassCredentialsRetriever dkbKeepassCredentialsRetriever = new KeepassCredentialsRetriever();
+        KeepassCredentialsRetriever credentialsRetriever = new KeepassCredentialsRetriever();
         AccountStatusCSVWriter accountStatusCSVWriter = new AccountStatusCSVWriter(dataDirectory);
 
-        for (LoginCredential loginCredential : dkbKeepassCredentialsRetriever.retrieveFor(BankingURL.QUIRION)) {
+        for (LoginCredential loginCredential : credentialsRetriever.retrieveFor(BankingURL.QUIRION)) {
             try (Browser browser = BrowserLauncher.createChromium()) {
                 BrowserContext context = browser.newContext();
 
-                QuirionPage page = new QuirionPage(context, loginCredential);
+                InstitutePage page = new QuirionPage(context, loginCredential);
                 page.open();
                 page.login();
                 List<AccountStatus> accountStatuses = page.fetchAccountData();
