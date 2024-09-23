0bffbe223caddf60627645088cf8e390c4773089
Lars K.W. Gohlke <lkwg82@gmx.de>
2024-09-23 07:44:45 +0200

    handle not found login credential

--- src/main/java/de/lgohlke/homebanking/keepass/KeepassCredentialsRetriever.java
+++ src/main/java/de/lgohlke/homebanking/keepass/KeepassCredentialsRetriever.java
@@ -2,10 +2,12 @@ package de.lgohlke.homebanking.keepass;
 
 import de.lgohlke.homebanking.LoginCredential;
 import de.lgohlke.homebanking.institutes.BankingURL;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 public class KeepassCredentialsRetriever {
 
     public List<LoginCredential> retrieveFor(BankingURL bankingURL) {
@@ -13,7 +15,10 @@ public class KeepassCredentialsRetriever {
         Map<String, Object> logins = retriever.retrieveLoginsForUrl(bankingURL.getUrl());
 
         @SuppressWarnings("unchecked") var entries = (List<Map<String, Object>>) logins.get("entries");
-
+        if (logins.isEmpty()) {
+            log.warn("no entries found for url {}", bankingURL.getUrl());
+            return List.of();
+        }
         return entries.stream()
                       .map(entry -> {
                           var login = (String) entry.get("login");
