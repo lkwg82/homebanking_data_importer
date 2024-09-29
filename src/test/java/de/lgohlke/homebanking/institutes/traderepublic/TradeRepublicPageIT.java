caa72b558908dd422fa47f5e712951a02d90e3b3
Lars K.W. Gohlke <lkwg82@gmx.de>
2024-09-29 15:15:05 +0200

    adds playwright in CI (#2)
    * wip
    
    * set specific maven version
    
    * wip
    
    * uses headless browser in tests
    
    * skip building shaded jar during tests
    
    * introduce spring boot parent pom to get deps managed
    
    * update spring boot parent pom
    
    * use dep management from parent pom
    
    * adds xvfb
    
    * adds xvfb
    
    * removes xvfb
    
    * cleanup
    
    * cleanup

--- src/test/java/de/lgohlke/homebanking/institutes/traderepublic/TradeRepublicPageIT.java
+++ src/test/java/de/lgohlke/homebanking/institutes/traderepublic/TradeRepublicPageIT.java
@@ -8,22 +8,33 @@ import de.lgohlke.homebanking.LoginCredential;
 import de.lgohlke.homebanking.institutes.InstitutePage;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class TradeRepublicPageIT {
     @Test
     void test_login_Page() {
-        try (Browser browser = BrowserLauncher.createChromium()) {
-            BrowserContext context = browser.newContext();
-            LoginCredential credential = new LoginCredential("name", "password");
-            InstitutePage iPage = new TradeRepublicPage(context, credential);
+        try (Browser browser = BrowserLauncher.createHeadlessChromium()) {
+            Browser.NewContextOptions options = new Browser.NewContextOptions();
+            // needed to run headless
+            String userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36";
+            options.setUserAgent(userAgent);
 
-            iPage.open(); // action
+            try (BrowserContext context = browser.newContext(options)) {
+                LoginCredential credential = new LoginCredential("name", "password");
+                InstitutePage iPage = new TradeRepublicPage(context, credential);
 
-            Page page = iPage.getPage();
-            String headline = page.locator(".loginPhoneNumber h2").textContent();
+                iPage.open(); // action
 
-            assertThat(headline).isEqualTo("Gib deine Telefonnummer ein");
+                Page page = iPage.getPage();
+                String headline = page.locator(".loginPhoneNumber h2").textContent();
+
+                assertThat(headline).isIn(List.of(
+                        "Gib deine Telefonnummer ein", // deutsch
+                        "Log in with your phone number" // english
+                ));
+            }
         }
     }
 }
